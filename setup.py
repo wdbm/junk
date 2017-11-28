e201ec30db1d0b2466f0710ef708a0694c2f4da0
Will Breaden Madden <wbm@protonmail.ch>
2017-11-28 16:22:40 +0000

    testing setup.py multiple modules

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def main():
 
     setuptools.setup(
         name             = "supermodule",
-        version          = "2015.10.30.1634",
+        version          = "2017.11.28.1622",
         description      = "super utilities",
         #long_description = (read("README.md")),
         long_description = pypandoc.convert("README.md", "rst"),
@@ -23,9 +23,10 @@ def main():
         license          = "GPLv3",
         py_modules       = ["supermodule"],
         entry_points     = """
-            [console_scripts]
-            supermodule = supermodule:supermodule
-        """
+                           [console_scripts]
+                           supermodule = supermodule:supermodule
+                           superdupermodule = superdupermodule:superdupermodule
+                           """
     )
 
 if __name__ == "__main__":
